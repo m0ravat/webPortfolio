500b4cc34f1ae10f387933cf80119c818cad37bf
Muhammad Ravat <m0ravat736@gmail.com>
2024-04-30 17:20:22 +0100

    cursors

--- JS/logic.js
+++ JS/logic.js
@@ -9,6 +9,7 @@ var icon = document.querySelectorAll("icon-text");
 var user = document.getElementById("userpImg");
 var active = document.getElementById("active");
 var topbox = document.getElementById("topbox");
+var cursor = document.getElementById("cursors");
 
 function menuclick() {
     if (count % 2 == 1) {
@@ -62,5 +63,40 @@ function load(){
     setTimeout(function(){document.getElementById("resume").style.left="0";}, 700);
     setTimeout(function(){document.getElementById("forms").style.visibility="visible";}, 700);
     setTimeout(function(){document.getElementById("forms").style.left="0";}, 700);
+}
+function cursorChange(){
+    let index = document.getElementById("cursors").selectedIndex;
+    switch(index){
+        case 0:
+            document.body.style.cursor="auto";
+            break;
+        case 1:
+            document.body.style.cursor= "url('https://sweezy-cursors.com/wp-content/uploads/cursor/star-wars-obi-wan-kenobi-lightsaber-animated/star-wars-obi-wan-kenobi-lightsaber-animated-pointer-32x32.png'),auto";
+            break;
+        case 2:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/star-wars-darth-vader-lightsaber/star-wars-darth-vader-lightsaber-cursor-32x32.png'),auto";
+            break;
+        case 3:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/kaiju-no-8-kafka-hibino-animated/kaiju-no-8-kafka-hibino-animated-cursor-3-32x32.png'),auto";
+            break;
+        case 4:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/black-rainbow-stroke-gradient-animated/black-rainbow-stroke-gradient-animated-cursor-8-32x32.png'),auto";
+            break;
+        case 5:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/one-piece-monkey-d-luffy-gear-4-animated/one-piece-monkey-d-luffy-gear-4-animated-cursor-2-32x32.png'),auto";
+            break;
+        case 6:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/lamborghini-veneno/lamborghini-veneno-cursor-32x32.png'),auto";
+            break;
+        case 7:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/red-laferrari/red-laferrari-cursor-32x32.png'),auto";
+            break;
+        case 8:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/auto-draft/purple-toyota-gr-supra-cursor-32x32.png'),auto";
+            break;
+        case 9:
+            document.body.style.cursor="url('https://sweezy-cursors.com/wp-content/uploads/cursor/among-us-john-wick-skin/among-us-john-wick-skin-cursor-32x32.png'),auto";
+            break;
+    }
 }
 
